4e3ea4c1443749eee97966aa729dcd4f1ff3d3e6
winescout <matt@notch8.com>
2019-10-15 20:23:13 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Greeter from './Greeter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      form:{
+        greeting: '',
+        email: ''
+      }
+    }
+  }
+  handleSubmit = ()=>{
+    var confirmed = window.confirm(`Is your name really ${this.state.form.greeting}`)
+    console.log("this was confirmed?", this.state.form)
+  }
+  handleChange = (e) => {
+    const{ form } = this.state 
+    form[e.target.name] = e.target.value
+    this.setState({form})
+  }
+  render(){
+    return (
+      <div className="App">
+        <label>Greeting</label>
+        <input
+          type="text"
+          name="greeting"
+          onChange={this.handleChange}
+          value = {this.state.form.name}
+          />
+        <br />
+        <label>Email</label>
+        <input
+          type='text'
+          name='email'
+          onChange={this.handleChange}
+          value={this.state.form.email}
+          />
+          <button
+            onClick={this.handleSubmit}
+          >
+            Submit Value
+          </button>
+          
+          <br />
+          <Greeter name={this.state.form.name} />
+      </div>
+    );
+  }
 }
 
 export default App;
